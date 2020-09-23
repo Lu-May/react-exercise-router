344e1f8130f5887d02966e99cb56f0628307805a
Lu-May <1054998345@qq.com>
2020-09-23 22:09:04 +0800

    [luyuehuan] feat:create Home Page

--- src/exercise-1/components/App.js
+++ src/exercise-1/components/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import '../styles/App.css';
 import {BrowserRouter as Router} from 'react-router-dom';
+import Home from './Home';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <Router />
+        <Home />
       </div>
     );
   }
---
+++ src/exercise-1/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = (props) => {
+    console.log(props);
+    return (
+    <section><p>This is a beautiful Home Page.</p>
+    <p>And the url is '/'.</p></section>
+    )
+};
+
+export default Home;
